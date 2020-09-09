6ad5b77524a738ba1fe91b864298704ee8d3c0cd
Rafael Azuero Hurtado <razueroh@gmail.com>
2020-09-09 17:59:37 -0500

    Refactor result component

--- src/client/containers/ResultList.js
+++ src/client/containers/ResultList.js
@@ -3,28 +3,12 @@ import PropTypes from 'prop-types';
 
 import Result from '../components/Result';
 import '../assets/styles/containers/ResultList.scss';
-import { getCurrency, formatAmount } from '../utils/price';
-import getCondition from '../utils/condition';
 
 const ResultList = ({ results }) => {
   return (
     <div className='results container pos-2 width-10'>
       {results.map((result) => {
-        const { id, title, price, condition, location, picture: thumbnail, free_shipping: isFreeShipping } = result;
-
-        return (
-          <Result
-            key={`ml-result-${id}`}
-            id={id}
-            thumbnail={thumbnail}
-            currency={getCurrency(price.currency)}
-            value={formatAmount(price.amount)}
-            title={title}
-            condition={getCondition(condition)}
-            location={location}
-            isFreeShipping={isFreeShipping}
-          />
-        );
+        return <Result key={`ml-result-${result.id}`} result={result} />;
       })}
     </div>
   );
